14b0859aa6d45f8f80a9e18c0a2e91ce48f06280
hossein_ghobadi <hossein.ghobadi894301149@gmail.com>
2025-11-05 10:30:53 +0330

    correct first looking design

--- Endpoint.Site/Program.cs
+++ Endpoint.Site/Program.cs
@@ -91,7 +91,7 @@ app.UseEndpoints(endpoints =>
         pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
     endpoints.MapControllerRoute(
         name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
+        pattern: "{controller=Projects}/{action=Index}/{id?}");
 });
 
 app.Run();
